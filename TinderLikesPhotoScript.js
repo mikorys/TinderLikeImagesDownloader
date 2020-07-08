b1dd6bede236679c2ed93c7d99de411d13a6cdc0
Adam Stachowicz <saibamenppl@gmail.com>
2020-07-08 17:52:42 +0200

    Formatting
    ESLinting

--- TinderLikesPhotoScript.js
+++ TinderLikesPhotoScript.js
@@ -5,23 +5,22 @@ const Path = require('path')
 const FsExtra = require('fs-extra')
 
 // !!!!!!!!!! Paste here X-Auth token below, XauthToken Should be betwen quotes '' for instance 'pasted X-authToken' !!!!!!!!!!
-let XAuth = 'PASTE X-AUTH TOKEN HERE' 
-// !! REMEMBER !!! personal X-AUTH TOKEN changes once a week if script does not work check if it is correct  
+let XAuth = 'PASTE X-AUTH TOKEN HERE'
+// !! REMEMBER !!! personal X-AUTH TOKEN changes once a week if script does not work check if it is correct
 
-const directory = __dirname + '/downloadedImages'
+const directory = `${__dirname}/downloadedImages`
 const tinderApiURL = 'https://api.gotinder.com/v2/fast-match/teasers'
 let config = {
     headers: {
-        'X-Auth-Token': `${XAuth}`
-    }
+        'X-Auth-Token': `${XAuth}`,
+    },
 }
 
 function createDownloadFolder() {//if no subDirectory exists create one
-    if (!Fs.existsSync(directory)){
+    if (!Fs.existsSync(directory)) {
         Fs.mkdirSync(directory);
-        console.log("Download subFolder successfully created!")
+        console.log('Download subFolder successfully created!')
     }
-
 }
 
 async function deleteAllImages() {//remove all images from driectory - using FS-Extra module
@@ -40,7 +39,7 @@ async function download(url, photoNo) {//main function download 10 last like ima
     const response = await Axios({
         method: 'GET',
         url: url,
-        responseType: 'stream'
+        responseType: 'stream',
     })
 
     response.data.pipe(Fs.createWriteStream(path))
@@ -56,21 +55,19 @@ async function download(url, photoNo) {//main function download 10 last like ima
     })
 }
 
-
  function getData() {
     createDownloadFolder()
     deleteAllImages()
     Axios.get(tinderApiURL, config)
         .then(response => {
-            var count = Object.keys(response.data.data.results).length
-            var imageUrl= ''
-            for (let i=0; i<count;i++){
-                imageUrl= response.data.data.results[i].user.photos[0].url
-                download(imageUrl,i+1)
+            let count = Object.keys(response.data.data.results).length
+            let imageUrl = ''
+            for (let i = 0; i < count; i++) {
+                imageUrl = response.data.data.results[i].user.photos[0].url
+                download(imageUrl, i + 1)
             }
         })
         .catch(err => console.log(err, err.response))
-
 }
 
 getData()
